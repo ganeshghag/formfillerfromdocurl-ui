2d7235de4927f635a810f4faf1b9eef8711be8cf
ganeshghag <ganesh.ghag@gmail.com>
2020-09-11 20:47:52 +0530

    working reasonably well, first cut

--- src/app/select-overview-example.ts
+++ src/app/select-overview-example.ts
@@ -17,7 +17,7 @@ interface Food {
 })
 export class SelectOverviewExample {
   foods: Food[] = [];
-  SERVER_URL = 'https://ganeshghag.pythonanywhere.com/';
+  SERVER_URL = 'https://ganeshghag.pythonanywhere.com/extract';
 
 
   constructor(private httpClient: HttpClient) { 
@@ -27,46 +27,42 @@ export class SelectOverviewExample {
   fetchData(url: string): void {
     url = url.trim();
     if (!url) { return; }
+    //const headers1 = new HttpHeaders().set('Content-Type', 'text/plain; charset=utf-8');
+    //this.httpClient.get(url,{headers: headers1, responseType:'text'}).subscribe((res)=>{
+    //console.log(res);
+    //  this.extractDataInternal(JSON.stringify(res));
+    //});
 
-
-      const headers1 = new HttpHeaders().set('Content-Type', 'text/plain; charset=utf-8');
-   
-      this.httpClient.get(url,{headers: headers1, responseType:'text'}).subscribe((res)=>{
-      console.log(res);
-      this.extractDataInternal(JSON.stringify(res));
-    });
+    this.extractDataInternal(url);
     
     //'James 9823456567 paid on 25/12/2020 and 1,234.23 to ganesh@email.com'
     
   }
 
-  private extractDataInternal(data: string){
-    this.sendPostRequest(data).subscribe(
+  private extractDataInternal(url: string){
+    this.sendGetRequest(url).subscribe(
       res => {
         console.log('RESP IS '+JSON.stringify(res));
-        var emails = res[0];        var mobiles = res[1];
-        var dates = res[2];        var amounts = res[3];
-        var names = res[4];
-        console.log(this.foods);console.log(emails);console.log(mobiles);console.log(dates);console.log(amounts);console.log(names);
+        var emails = res[0];        
+        var dates = res[1];        
+        var names = res[2];
+        var mobiles = res[3];
+
         emails.forEach(elem => this.foods.push({value: elem, viewValue: elem}));  
-        mobiles.forEach(elem => this.foods.push({value: elem, viewValue: elem}));  
         dates.forEach(elem => this.foods.push({value: elem, viewValue: elem}));  
-        //amounts.forEach(elem => this.foods.push({value: elem, viewValue: elem}));  
         names.forEach(elem => this.foods.push({value: elem, viewValue: elem}));  
+        mobiles.forEach(elem => this.foods.push({value: elem, viewValue: elem}));  
+
       });
   }
 
   ngOnInit() {
     console.log('from ngInit GG ');
-
-        //this.foods = res[0];
   }
 
-
-  private sendPostRequest(data: any): Observable<any> {
-     return this.httpClient.post<any>(this.SERVER_URL, data);
+  private sendGetRequest(url: any): Observable<any> {
+     return this.httpClient.get<any>(this.SERVER_URL+'?myurl='+url);
   }
-
 
 }
 
